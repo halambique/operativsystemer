7860150300b379707f108ef33d0a34fb6bda1a54
halambique <mathias@mbq.no>
2023-09-08 16:21:08 +0200

    commit

--- Intro2OS/lab_02/oppgave_2.c
+++ Intro2OS/lab_02/oppgave_2.c
@@ -4,17 +4,22 @@
 
 int main() {
     int array[1000];
-    int i, max;
+    int i, max = 0, min = 10000;
 
     srand(time(NULL));
 
-    for (i = 1; i < 1000; i++) {
+    for (i = 0; i < 1000; i++) {
         array[i] = rand() % 100001;
         if (array[i] > max) {
             max = array[i];
         }
+        if (array[i] < min){
+            min = array[i];
+        }
     }
 
-    printf("Det største tallet i arrayet er: %d", max);
+    printf("Det største tallet i arrayet er: %d\n", max);
+    printf("Det minste tallet i arrayet er: %d\n", min);
+
     return 0;
 }
